e55bc7c7f84b469f7ee1915635c705c7393aa2fd
Andres <andres.rivas@mercadolibre.com>
2020-08-15 13:59:46 -0300

    Fragment manager added

--- app/src/main/java/com/ae/tabbedmovies/ui/popularmovies/view/PopularMoviesFragment.kt
+++ app/src/main/java/com/ae/tabbedmovies/ui/popularmovies/view/PopularMoviesFragment.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.ae.tabbedmovies.R
 import com.ae.tabbedmovies.ui.popularmovies.viewmodel.PageViewModel
 
@@ -20,42 +17,15 @@ class PopularMoviesFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
-            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
-        }
+
     }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_main, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
-        pageViewModel.text.observe(this, Observer<String> {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_popular_movies, container, false)
     }
 
-    companion object {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private const val ARG_SECTION_NUMBER = "section_number"
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        @JvmStatic
-        fun newInstance(sectionNumber: Int): PopularMoviesFragment {
-            return PopularMoviesFragment()
-                .apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_SECTION_NUMBER, sectionNumber)
-                }
-            }
-        }
-    }
 }
