abfa3aa79989bbb512e14e639d00c06456798470
Andres <andres.rivas@mercadolibre.com>
2020-08-18 18:03:47 -0300

    layout, viewmodel koin implementation added

--- app/src/main/java/com/ae/tabbedmovies/core/di/ViewModelModule.kt
+++ app/src/main/java/com/ae/tabbedmovies/core/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package com.ae.tabbedmovies.core.di
 import com.ae.tabbedmovies.data.service.MoviesServices
 import com.ae.tabbedmovies.ui.popularmovies.viewmodel.PopularMoviesViewModel
 import com.ae.tabbedmovies.ui.topdated.viewmodel.TopRatedViewModel
+import com.ae.tabbedmovies.ui.upcoming.viewmodel.UpComingViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -19,4 +20,9 @@ val viewModelModule: Module = module {
      * Provide TopRatedMoviesViewModel
      */
     viewModel<TopRatedViewModel> { TopRatedViewModel(get<MoviesServices>()) }
+
+    /**
+     * Provide TopRatedMoviesViewModel
+     */
+    viewModel<UpComingViewModel> { UpComingViewModel(get<MoviesServices>()) }
 }
--- app/src/main/java/com/ae/tabbedmovies/ui/upcoming/viewmodel/UpComingViewModel.kt
+++ app/src/main/java/com/ae/tabbedmovies/ui/upcoming/viewmodel/UpComingViewModel.kt
@@ -1,6 +1,7 @@
 package com.ae.tabbedmovies.ui.upcoming.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.ae.tabbedmovies.data.service.MoviesServices
 
-class UpComingViewModel: ViewModel() {
+class UpComingViewModel(private val moviesServices: MoviesServices): ViewModel() {
 }
